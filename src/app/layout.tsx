32253bb0121a58fef03de8d51ef3cdb89f5a2b8c
GiFTED <gkenny896@gmail.com>
2025-10-07 04:36:55 +0100

    more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,52 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Nickels.vc",
-  description: "Nickels.vc -- Micro-funding, $0.05 at a time",
+  description:
+    "nickels.vc - Ultra-Early Stage Venture Capital | Micro-funding, $0.05 at a time",
+  keywords: [
+    "venture capital",
+    "VC",
+    "early stage",
+    "ultra-early stage",
+    "micro-funding",
+    "startup funding",
+    "seed funding",
+    "angel investing",
+    "startup investment",
+    "pre-seed",
+    "micro VC",
+    "startup accelerator",
+    "startup incubator",
+  ],
+  authors: [
+    {
+      name: "_0xGifted",
+      url: "https://x.com/_0xgifted",
+    },
+  ],
+  creator: "_0xGifted",
+  openGraph: {
+    title: "nickels.vc",
+    description:
+      "nickels.vc - Ultra-Early Stage Venture Capital | Micro-funding, $0.05 at a time",
+    url: "https://nickels.vc",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "nickels.vc",
+    description:
+      "nickels.vc - Ultra-Early Stage Venture Capital | Micro-funding, $0.05 at a time",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  alternates: {
+    canonical: "https://nickels.vc",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +72,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <Script
           crossOrigin="anonymous"
           src="//unpkg.com/same-runtime/dist/index.global.js"
